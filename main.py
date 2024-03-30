9c1056858c063b77414b362926b6110c32eaded8
Kaixhin <design@kaixhin.com>
2024-03-30 12:44:35 +0900

    Implement background removal needed for model

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import numpy as np
 from PIL import Image
+from rembg import remove
 import torch
 
 sys.path.append("TripoSr")
@@ -22,7 +23,11 @@ async def convert_2d_image_to_3d(image: UploadFile = File(...)):
   filename = os.path.splitext(image.filename)[0]  # Strip extension
   contents = await image.read()
   image = np.array(Image.open(BytesIO(contents)).convert("RGB"))
+  image = remove(image).astype(np.float32) / 255.0  # Remove background (alpha) and cast to float
+  image = image[:, :, :3] * image[:, :, 3:4] + (1 - image[:, :, 3:4]) * 0.5  # Convert from RGBA to RGB with gray background
+
   scene_codes = model([image], device='cpu')
   meshes = model.extract_mesh(scene_codes, resolution=256)
   meshes[0].export(os.path.join("cache", f"{filename}.obj"))
+  
   return FileResponse(os.path.join("cache", f"{filename}.obj"), media_type='application/octet-stream', filename=f"{filename}.obj")
